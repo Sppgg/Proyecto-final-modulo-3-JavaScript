69480a9da03b8bbe126e50fbc2f3727511e4d549
Sppgg <saracodestheweb@gmail.com>
2025-01-29 23:25:55 +0000

    resolved exercises 7 and 8, plus done extra changes

--- Algoritmos/Ejercicios/ejercicio7.js
+++ Algoritmos/Ejercicios/ejercicio7.js
@@ -1,1 +1,14 @@
-console.log("baby hellooo");
+// 7 - Ordenar un array de números en orden ascendente (sin usar sort).
+
+let arrNum = [1, 5, 46, 8, 64, 2]; 
+
+for (let j = 0; j < arrNum.length - 1; j++) {  // Repetimos el proceso varias veces
+    for (let i = 0; i < arrNum.length - 1; i++) {  // Recorremos el array
+        if (arrNum[i] > arrNum[i + 1]) {  // Si están en el orden incorrecto
+            let temp = arrNum[i];  // Guardamos el número en una variable temporal
+            arrNum[i] = arrNum[i + 1];  // Intercambiamos los valores
+            arrNum[i + 1] = temp;
+        }
+    }
+}
+console.log(arrNum);
--- Algoritmos/Ejercicios/ejercicio8.js
+++ Algoritmos/Ejercicios/ejercicio8.js
@@ -1,1 +1,18 @@
-console.log("baby hellooo");
+// 8 - Crear una función que cuente cuántas veces aparece un carácter en un string.
+
+let input = prompt("introduce una palabra para comprobar cuántas veces aparece un carácter:");
+let caracter = prompt("Introduce el carácter que quieres contar:");
+function caracterCount (input, caracter) {
+    let counter = 0;
+    if (typeof input!=="string"|| input.trim() === "") {
+        console.log("Esto no es una palabra, pillín");
+    } else {
+        for (let i = 0; i < input.length; i++) {
+            if (input[i] === caracter) {
+                counter++;
+        }        
+    }
+    console.log(`El carácter "${caracter}" aparece ${counter} veces en la cadena.`);
+}
+}
+caracterCount(input, caracter);
