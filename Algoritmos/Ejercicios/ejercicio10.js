af7002f4ef27da85f9373bc3b3eda28a83f31bc4
Sppgg <saracodestheweb@gmail.com>
2025-01-31 20:52:16 +0000

    Resolved exercises 9 and 10

--- Algoritmos/Ejercicios/ejercicio10.js
+++ Algoritmos/Ejercicios/ejercicio10.js
@@ -1,1 +1,30 @@
-console.log("baby hellooo");
+// 10 - Crear un programa que convierta grados Celsius a Fahrenheit y viceversa. 
+
+// Pedimos al usuario que elija qué conversión quiere hacer
+let tipo =  prompt("¿Quieres convertir de Celsius a Fahrenheit o de Fahrenheit a Celsius? Escribe 'C' o 'F'");
+
+// Normalizamos la entrada para evitar errores de mayúsculas/minúsculas
+tipo = tipo.toUpperCase();
+
+// Evaluamos la opción elegida
+if (tipo === "C") {
+    let celsius = parseFloat(prompt("Ingresa la temperatura en grados Celsius:"));
+    if (isNaN(celsius)){
+        console.log("¿En qué planeta eso son números?");
+    } else{
+        let fahrenheit = (celsius * 9/5) + 32;
+        console.log(`${celsius}°C son ${fahrenheit.toFixed(2)}°F`);   
+    }
+} 
+else if (tipo === "F") {
+    let fahrenheit = parseFloat(prompt("Ingresa la temperatura en grados Fahrenheit:"));
+    if (isNaN(fahrenheit)) {
+        console.log("¿En qué planeta eso son números?");
+    } else {
+        let celsius = (fahrenheit - 32) * 5/9;
+        console.log(`${fahrenheit}°F son ${celsius.toFixed(2)}°C`); 
+    }
+} 
+else {
+    console.log("Opción no válida, pillín Debes escribir 'C' o 'F'.");
+}
--- Algoritmos/Ejercicios/ejercicio9.js
+++ Algoritmos/Ejercicios/ejercicio9.js
@@ -1,1 +1,18 @@
-console.log("baby hellooo");
+// 9 - Escribir un algoritmo que determine si un año es bisiesto.
+
+/* Un año es bisiesto si cumple ciertas condiciones:
+Un año es bisiesto si es divisible por 4.
+Pero si el año también es divisible por 100, entonces no es bisiesto.
+A menos que también sea divisible por 400. En ese caso, sí es bisiesto.*/
+
+let año = parseInt(prompt("Introduce un año:"));
+
+if (año % 400 === 0) {
+    console.log("Es un año bisiesto");
+} else if (año % 100 === 0) {
+    console.log("No es un año bisiesto");
+} else if (año % 4 === 0) {
+    console.log("Es un año bisiesto");
+} else {
+    console.log("No es un año bisiesto");
+}
