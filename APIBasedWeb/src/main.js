54a063dc056d5451872502dd4e9b56929ff183c9
Sppgg <saracodestheweb@gmail.com>
2025-02-03 16:51:26 +0000

    fixed GET

--- APIBasedWeb/src/main.js
+++ APIBasedWeb/src/main.js
@@ -33,31 +33,6 @@ async function getAllProducts() {
 getAllProducts().then(products => console.log(products));
 // getAllProducts por sí solo en un console log imprimirá una promesa pendiente. usando .then(+arrow function) espera a que la Promesa se resuelva y luego imprime los datos.
 
-async function renderProducts() {
-  const products = await getAllProducts();
-  const container = document.getElementById("products-container");
-
-  container.innerHTML = ""; // Limpia el contenido antes de agregar nuevos productos
-
-  products.forEach(products => {
-    const productElement = document.createElement("div");
-    productElement.classList.add("product");
-
-    productElement.innerHTML = `
-      <img src="${product.image}" alt="${product.title}" width="100">
-      <h3>${product.title}</h3>
-      <p>Precio: $${product.price}</p>
-      <p>${product.description}</p>
-      <button>Editar</button>
-      <button>Eliminar</button>
-    `;
-
-    container.appendChild(productElement);
-  });
-}
-
-// Llamamos a la función para que se ejecute al cargar la página
-renderProducts();
 
 //  --------------- PETICIÓN POST ---------------
 //  --------------- PETICIÓN PUT ---------------
