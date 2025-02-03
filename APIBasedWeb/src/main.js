62b55290b0c642aac99f2677d084c4c8a1465038
Sppgg <saracodestheweb@gmail.com>
2025-02-03 16:47:23 +0000

    started CRUD + GET petition done

--- APIBasedWeb/src/main.js
+++ APIBasedWeb/src/main.js
@@ -1,24 +1,64 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const API_BASE_URL = "https://fakestoreapi.com";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/* COMENZAMOS
+CON
+EL 
+C R U D */
 
-setupCounter(document.querySelector('#counter'))
+//  --------------- PETICIÓN GET ---------------
+async function getAllProducts() {
+  let url = `${API_BASE_URL}/products`;
+  try {
+    const response = await fetch (url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error inesperado: ${response.status}`);
+    } else{
+      const products = await response.json(); 
+      console.log("Lista de productos:", products);
+    }
+
+  } catch (error) {
+    console.error("Error al obtener los productos:", error);
+    return []; 
+    // Devuelve un array vacío en caso de error para evitar fallos en la UI
+  }
+}
+
+// Llamar la función y probar en consola
+getAllProducts().then(products => console.log(products));
+// getAllProducts por sí solo en un console log imprimirá una promesa pendiente. usando .then(+arrow function) espera a que la Promesa se resuelva y luego imprime los datos.
+
+async function renderProducts() {
+  const products = await getAllProducts();
+  const container = document.getElementById("products-container");
+
+  container.innerHTML = ""; // Limpia el contenido antes de agregar nuevos productos
+
+  products.forEach(products => {
+    const productElement = document.createElement("div");
+    productElement.classList.add("product");
+
+    productElement.innerHTML = `
+      <img src="${product.image}" alt="${product.title}" width="100">
+      <h3>${product.title}</h3>
+      <p>Precio: $${product.price}</p>
+      <p>${product.description}</p>
+      <button>Editar</button>
+      <button>Eliminar</button>
+    `;
+
+    container.appendChild(productElement);
+  });
+}
+
+// Llamamos a la función para que se ejecute al cargar la página
+renderProducts();
+
+//  --------------- PETICIÓN POST ---------------
+//  --------------- PETICIÓN PUT ---------------
+//  --------------- PETICIÓN DELETE ---------------
